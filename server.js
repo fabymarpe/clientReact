275670edb9573e74828145f9480537e089404c5c
fmartinez <fmartinez@campus.udes.edu.co>
2018-07-28 21:41:46 -0500

    Conflicts solved.

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const app = express();
 
 // Since the root/src dir contains our index.html
 app.use(express.static(__dirname + '/src/'));
+app.get('*', (req, res) => res.sendFile(path.resolve('src', 'index.html'));
 
 // Heroku bydefault set an ENV variable called PORT=443
 //  so that you can access your site with https default port.
