e122f8b9c3eb2681c5adf2a40af74080decd8a85
fmartinez <fmartinez@campus.udes.edu.co>
2018-07-28 21:04:55 -0500

    + Conflicts solved

--- server.js
+++ server.js
@@ -1,26 +1,14 @@
-import express from 'express';
-import path from 'path';
-import open from 'open';
-import compression from 'compression';
-import favicon from 'serve-favicon';
+var express = required('express');
 
-/*eslint-disable no-console */
-
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 const app = express();
 
-app.use(compression());
-app.use(express.static('public'));
-app.use(favicon(path.join(__dirname,'assets','public','favicon.ico')));
+// this is to serve the frontend part
+app.use(express.static('build/'));
 
-app.get('*', function(req, res) {
-    res.sendFile(path.join(__dirname, '../public/index.html'));
-});
+// your backend routes here
+app.get('/api/xyz', (req, res) => {});
 
-app.listen(port, function(err) {
-    if (err) {
-        console.log(err);
-    } else {
-        open(`http://localhost:${port}`);
-    }
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}!`);
 });
